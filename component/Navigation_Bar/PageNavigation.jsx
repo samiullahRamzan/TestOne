7b5a05c54a1b150c8d005d73c33b86fcee6c73bf
SamiUllah-dev336 <samidev336@gmail.com>
2024-06-01 13:28:22 +0500

    Icons display

--- component/Navigation_Bar/PageNavigation.jsx
+++ component/Navigation_Bar/PageNavigation.jsx
@@ -1,27 +1,15 @@
 import { View, StyleSheet, Image, Text,ScrollView } from 'react-native'
-import * as React from 'react'
-import PageNavigation_Style from './PageNavigation_Style.js';
+import * as React from 'react';
 
-import { enableScreens } from 'react-native-screens';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+//import { enableScreens } from 'react-native-screens';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { faHome, faBell, faPlus, faUser } from '@fortawesome/free-solid-svg-icons';
-import { FaHome } from "react-icons/fa";
-<<<<<<< HEAD
-import { NavigationContainer } from "@react-navigation/native";
-
+import Icon from 'react-native-vector-icons/Feather';
 import Home from "../Home/Home.jsx";
-=======
-
-import App from '../MainSlider/ImageSlider.jsx';
->>>>>>> 5b8ca503b3d1509cb3060ded693cf6d8b2778e46
 import Notification from '../Notification/Notification.jsx';
 import Post_Upload from '../Post_Upload/Post_Upload.jsx';
 import Profile from '../Profile/Profile.jsx';
 
-enableScreens();
+//enableScreens();
 const Tabs = createBottomTabNavigator();
 
 // const tabBarOptions = {
@@ -42,93 +30,36 @@ const Tabs = createBottomTabNavigator();
 
 const PageNavigation = () => {
 
+    
+    // screenOptions={{
+    //     // showlabel: false,
+    //     Style: {
+    //         position: 'absolute',
+    //         bottom: 25,
+    //         left: 20,
+    //         right: 20,
+    //         elevation: 0,
+    //         backgroundColor: '#ffffff',
+    //         borderRadius: 15,
+    //         height: 90,
+    //     }
+    // }
+    // } style={(styles.shadow)}
     return (
-<<<<<<< HEAD
-        <Tabs.Navigator screenOptions={{
-            // showlabel: false,
-            Style: {
-                position: 'absolute',
-                bottom: 25,
-                left: 20,
-                right: 20,
-                elevation: 0,
-                backgroundColor: '#ffffff',
-                borderRadius: 15,
-                height: 90,
-            }
-            //tabBarShowLabel: false,
-        }
-        } style={(styles.shadow)}>
-            <Tabs.Screen name="Home" component={Home}
-                options={{
-                    tabBarIcons: ({ focused }) => (
-                        <View style={{ alignItems: 'center', justifyContent: 'center', top: 10 }}>
-                            <Ionicons name="home" color={tintColor} size={25} />
-
-                            {/* <FontAwesomeIcon
-                        icon={faHome}
-                        size={25}
-                        color={focused ? '#e32f45' : '#748c94'}
-                    /> */}
-                            <Text style={{ color: focused ? '#e32f45' : '#748c94', fontSize: 12 }}>
-                                HOME
-                            </Text>
-                        </View>
-                    )
-                }}
+        <Tabs.Navigator>
+            <Tabs.Screen name="Home" component={Home} 
+                options={{headerShown:false,
+                      
+                        tabBarIcon: (tabinfo) => {
+                                return <Icon name='home' color={'purple'}  size={25}/>  
+                        }
+                   }} 
             />
             <Tabs.Screen name="Noti" component={Notification} />
             <Tabs.Screen name="Post_upload" component={Post_Upload} />
             <Tabs.Screen name="Profile" component={Profile} />
         </Tabs.Navigator>
              
-=======
-        <ScrollView virtical={true}>
-        <SafeAreaView style={{ flex: 1 }}>
-            {/* <View style={(styles.fixedBottomContent, styles.tabBarStyle)}> */}
-                <View style={{ flex: 1 }}>
-                    <Tabs.Navigator screenOptions={{
-                        // showlabel: false,
-                        Style: {
-                            position: 'absolute',
-                            bottom: 25,
-                            left: 20,
-                            right: 20,
-                            elevation: 0,
-                            backgroundColor: '#ffffff',
-                            borderRadius: 15,
-                            height: 90,
-                        }
-                        //tabBarShowLabel: false,
-                    }
-                    } style={(styles.shadow)}>
-                        <Tabs.Screen name="Home" component={App}
-                            options={{
-                                tabBarIcons: ({ focused }) => (
-                                    <View style={{ alignItems: 'center', justifyContent: 'center', top: 10 }}>
-                                        <Ionicons name="home" color={tintColor} size={25} />
-
-                                        {/* <FontAwesomeIcon
-                                    icon={faHome}
-                                    size={25}
-                                    color={focused ? '#e32f45' : '#748c94'}
-                                /> */}
-                                        <Text style={{ color: focused ? '#e32f45' : '#748c94', fontSize: 12 }}>
-                                            HOME
-                                        </Text>
-                                    </View>
-                                )
-                            }}
-                        />
-                        <Tabs.Screen name="Noti" component={Notification} />
-                        <Tabs.Screen name="Post_upload" component={Post_Upload} />
-                        <Tabs.Screen name="Profile" component={Profile} />
-                    </Tabs.Navigator>
-                </View>
-            {/* </View> */}
-        </SafeAreaView>
-        </ScrollView>
->>>>>>> 5b8ca503b3d1509cb3060ded693cf6d8b2778e46
     )
 }
 
